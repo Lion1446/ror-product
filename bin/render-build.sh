1b737f0cb14c1fb8a0c2a62fc9405e436cbbb6b4
Lionelle Diaz <lionellediaz@Lionelles-MacBook-Pro.local>
2024-06-13 17:53:04 +0800

    retried sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
-# Install dependencies
 bundle install
-
-# Precompile assets
 bundle exec rails assets:precompile
-
-# Clean up assets
 bundle exec rails assets:clean
