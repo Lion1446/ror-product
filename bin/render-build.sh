7fb1c831c853d6a24f29ef30a66fbb399554ea56
Lionelle Diaz <lionellediaz@Lionelles-MacBook-Pro.local>
2024-06-13 17:30:03 +0800

    fixed the error in shebang line

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash# exit on error
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean up assets
 bundle exec rails assets:clean
